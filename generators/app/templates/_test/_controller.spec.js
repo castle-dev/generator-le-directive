5dabd26852575acedac5792a9d8b36765a2b32e0
Tim Crowe <tim.crowe@entercastle.com>
2016-02-10 18:19:28 -0800

    feat(_controller.spec): add  to test

--- generators/app/templates/_test/_controller.spec.js
+++ generators/app/templates/_test/_controller.spec.js
@@ -4,8 +4,9 @@
   describe('common.controllers.<%= controllerName %> module', function() {
     var controller;
     beforeEach(module('common.controllers.<%= controllerName %>'));
-    beforeEach(inject(function($controller) {
-      controller = $controller('<%= controllerName %>', {});
+    beforeEach(inject(function($controller, $rootScope) {
+      var scope = $rootScope.$new();
+      controller = $controller('<%= controllerName %>', {$scope: scope});
     }));
 
     describe('<%= controllerName %>', function () {
